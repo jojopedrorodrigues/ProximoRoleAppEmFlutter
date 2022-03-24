8376e97e9508d1bb53d5197b0f34262dbc5d5de6
jojopedrorodrigues <jlabssistem@gmail.com>
2022-03-24 11:27:26 -0300

    commit2tela

--- lib/main.dart
+++ lib/main.dart
@@ -40,7 +40,7 @@ class MyApp extends StatelessWidget {
   }
 }
 
-String tipo = '';
+var tipo = '';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -64,6 +64,7 @@ class _HomeState extends State<Home> {
     _controller.play();
     if (_contador == 0) {
       setState(() {
+        role = "familia";
         tipo = 'Familia';
         _contador++;
       });
@@ -72,7 +73,8 @@ class _HomeState extends State<Home> {
     super.initState();
   }
 
-  _UpdateScreen() {
+  // ignore: non_constant_identifier_names
+  Future<int> _UpdateScreen() async {
     if (_contador == 1) {
       setState(() {
         _controller = VideoPlayerController.asset('img/namoro.mp4');
@@ -80,12 +82,13 @@ class _HomeState extends State<Home> {
           setState(() {});
         });
 
-        _controller.setLooping(true);
+        _controller.setLooping(false);
         _controller.play();
       });
       setState(() {
         _contador++;
-        tipo = 'Namoro';
+        tipo = 'Crush';
+        role = "namoro";
       });
 
       return 0;
@@ -97,11 +100,12 @@ class _HomeState extends State<Home> {
           setState(() {});
         });
 
-        _controller.setLooping(true);
+        _controller.setLooping(false);
         _controller.play();
       });
       setState(() {
         tipo = 'Amigos';
+        role = "amigos";
         _contador++;
       });
       return 0;
@@ -113,11 +117,12 @@ class _HomeState extends State<Home> {
           setState(() {});
         });
 
-        _controller.setLooping(true);
+        _controller.setLooping(false);
         _controller.play();
       });
       setState(() {
         tipo = 'Animal';
+        role = "amigos";
         _contador = 0;
       });
       return 0;
@@ -130,15 +135,17 @@ class _HomeState extends State<Home> {
           setState(() {});
         });
 
-        _controller.setLooping(true);
+        _controller.setLooping(false);
         _controller.play();
       });
       setState(() {
         tipo = 'Familia';
+        role = "familia";
         _contador++;
       });
       return 0;
     }
+    return 0;
   }
 
   @override
@@ -168,7 +175,7 @@ class _HomeState extends State<Home> {
           _iconsInicio(),
           inicioLayout(query, tipo),
           const Padding(padding: EdgeInsets.only(top: 25)),
-          _buttonAtualizar(query, _UpdateScreen)
+          _buttonAtualizar(query, _UpdateScreen, context)
         ]),
       ),
     );
@@ -176,71 +183,82 @@ class _HomeState extends State<Home> {
 }
 
 _iconsInicio() {
-  return Expanded(
-    child: Row(
-      mainAxisAlignment: MainAxisAlignment.end,
-      //crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: [
-        IconButton(
-            onPressed: () {},
-            icon: const Icon(
-              Icons.horizontal_split_outlined,
-              color: Colors.white,
-              size: 28,
-            ))
-      ],
-    ),
+  return Row(
+    children: [
+      Expanded(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          //crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.horizontal_split_outlined,
+                  color: Colors.white,
+                  size: 28,
+                ))
+          ],
+        ),
+      ),
+    ],
   );
 }
 
-_buttonAtualizar(MediaQueryData query, _UpdateScreen) {
-  assert(query != null);
-  return Expanded(
-    child: Column(
-      mainAxisAlignment: MainAxisAlignment.end,
-      children: [
-        Row(children: [
-          Center(
-            child: SizedBox(
-              width: query.size.width - 100,
-              height: 50,
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  primary: Colors.black, // background
-                  onPrimary: Colors.white, // foreground
-                ),
-                onPressed: () {
-                  _UpdateScreen();
-                }, //_UpdateScreen(),
-                child: const Text(
-                  'Trocar',
-                  style: TextStyle(color: Colors.white, fontSize: 17),
-                ),
-              ),
-            ),
-          ),
-          const Padding(padding: EdgeInsets.only(left: 3)),
-          Center(
-            child: SizedBox(
-              width: 97,
-              height: 50,
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  primary: Colors.white, // background
-                  onPrimary: Colors.black, // foreground
-                ),
-                onPressed: () {},
-                child: const Icon(
-                  Icons.arrow_circle_right,
-                  color: Colors.black,
-                  size: 22,
+// ignore: non_constant_identifier_names
+_buttonAtualizar(MediaQueryData query, UpdateScreen, BuildContext context) {
+  assert(UpdateScreen != null);
+  return Row(
+    children: [
+      Expanded(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            Row(children: [
+              Center(
+                child: SizedBox(
+                  width: query.size.width - 100,
+                  height: 50,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.black, // background
+                      onPrimary: Colors.white, // foreground
+                    ),
+                    onPressed: () async {
+                      await UpdateScreen();
+                    }, //_UpdateScreen(),
+                    child: const Text(
+                      'Trocar',
+                      style: TextStyle(color: Colors.white, fontSize: 17),
+                    ),
+                  ),
                 ),
               ),
-            ),
-          ),
-        ]),
-      ],
-    ),
+              const Padding(padding: EdgeInsets.only(left: 3)),
+              Center(
+                child: SizedBox(
+                  width: 97,
+                  height: 50,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.white, // background
+                      onPrimary: Colors.black, // foreground
+                    ),
+                    onPressed: () {
+                      Navigator.pushNamed(context, "/segundo");
+                    },
+                    child: const Icon(
+                      Icons.arrow_circle_right,
+                      color: Colors.black,
+                      size: 22,
+                    ),
+                  ),
+                ),
+              ),
+            ]),
+          ],
+        ),
+      ),
+    ],
   );
 }
 
@@ -276,8 +294,8 @@ inicioLayout(MediaQueryData query, String tipo) {
         child: Row(
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              const SizedBox(
+            children: const [
+              SizedBox(
                 height: 70,
                 child: Icon(
                   Icons.location_on_rounded,
@@ -285,7 +303,7 @@ inicioLayout(MediaQueryData query, String tipo) {
                   color: Colors.white,
                 ),
               ),
-              const Text(
+              Text(
                 'Mariana MG',
                 style: TextStyle(
                     fontSize: 17,
@@ -295,142 +313,5 @@ inicioLayout(MediaQueryData query, String tipo) {
             ]),
       )),
     ],
-  );
-}
-
-_update(String new_role, BuildContext context) {
-  role = new_role;
-  _new_page(context);
-}
-
-_new_page(BuildContext context) async {
-  Navigator.pushNamed(context, '/segundo');
-}
-
-Widget _blocoRole(BuildContext context) {
-  String new_role = 'rolegrupo';
-
-  return GestureDetector(
-    onTap: () => _update(new_role, context),
-    child: Card(
-      shape: BeveledRectangleBorder(
-        borderRadius: BorderRadius.circular(10.0),
-      ),
-      child: InkWell(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text(
-              'Decida o role em grupo',
-              style: TextStyle(color: Colors.black, fontSize: 17.5),
-            ),
-          ],
-        ),
-      ),
-      color: Colors.white,
-      elevation: 2.0,
-    ),
-  );
-}
-
-Widget _blocoNamoro(BuildContext context) {
-  const String new_role = 'rolenamoro';
-  return GestureDetector(
-    onTap: () => _update(new_role, context),
-    child: Card(
-      shape: BeveledRectangleBorder(
-        borderRadius: BorderRadius.circular(10.0),
-      ),
-      child: InkWell(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            SizedBox(
-              height: 141,
-              child: Image.asset(
-                'img/namorop.png',
-                fit: BoxFit.cover,
-              ),
-            ),
-          ],
-        ),
-      ),
-      color: Colors.white,
-      elevation: 1.0,
-    ),
-  );
-}
-
-Widget _blocoFamilia(BuildContext context) {
-  const String new_role = 'rolefamilia';
-  return GestureDetector(
-    onTap: () => _update(new_role, context),
-    child: Card(
-      shape: BeveledRectangleBorder(
-        borderRadius: BorderRadius.circular(2.0),
-      ),
-      child: InkWell(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            SizedBox(
-                height: 141,
-                child: Image.asset(
-                  'img/familiap.png',
-                  fit: BoxFit.cover,
-                )),
-          ],
-        ),
-      ),
-      elevation: 2.0,
-    ),
-  );
-}
-
-Widget _blocoAmigos(BuildContext context) {
-  MediaQueryData query = MediaQuery.of(context);
-  const String new_role = 'roleamigos';
-  return GestureDetector(
-    onTap: () => _update(new_role, context),
-    child: Card(
-      shape: BeveledRectangleBorder(
-        borderRadius: BorderRadius.circular(2.0),
-      ),
-      color: Colors.red[900],
-      child: SizedBox(
-        height: 141,
-        child: Image.asset(
-          'img/amigosr.png',
-          fit: BoxFit.cover,
-        ),
-      ),
-      elevation: 1.0,
-    ),
-  );
-}
-
-Widget _blocoAnimal(BuildContext context) {
-  const String new_role = 'roleanimal';
-  return Card(
-    shape: BeveledRectangleBorder(
-      borderRadius: BorderRadius.circular(2.0),
-    ),
-    child: InkWell(
-      onTap: () => _update(new_role, context),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          SizedBox(
-            height: 141,
-            child: Image.asset(
-              'img/animalp.png',
-              fit: BoxFit.cover,
-            ),
-          ),
-        ],
-      ),
-    ),
-    color: Colors.white,
-    elevation: 1.0,
   );
 }
--- lib/page_role/page.dart
+++ lib/page_role/page.dart
@@ -1,5 +1,5 @@
 import 'dart:math';
-
+import 'package:google_fonts/google_fonts.dart';
 import 'package:flutter/material.dart';
 import 'package:proximorole/main.dart';
 import 'package:flutter_share/flutter_share.dart';
@@ -22,7 +22,6 @@ class Page_ extends StatefulWidget {
 class _Page_State extends State<Page_> {
   @override
   void initState() {
-    _imagereturn();
     super.initState();
   }
 
@@ -35,256 +34,67 @@ class _Page_State extends State<Page_> {
         chooserTitle: 'Baixe o aplicativo Proximo Role voce tambem');
   }
 
-  _variavel() {
-    if (widget.cont == 0) {
-      _imagereturn();
-      widget.roledehoje = _gerador(role);
-      return widget.roledehoje;
-    } else {
-      setState(() {
-        _imagereturn();
-        widget.varia = _gerador(role);
-        widget.roledehoje = widget.varia;
-      });
-      return widget.varia;
-    }
-  }
-
-  _imagereturn() {
-    setState(() {
-      switch (widget.role) {
-        case 'rolefamilia':
-          setState(() {
-            widget.img = 'img/senhora.gif';
-            widget.frase = 'Pegue a sugestao da Dona Cleusa';
-          });
-
-          break;
-        case 'roleanimal':
-          setState(() {
-            widget.img = 'img/cachorro.gif';
-            widget.frase = 'AuAu! traduzido  em sugestao';
-          });
-
-          break;
-        case 'rolenamoro':
-        case 'roleamigos':
-      }
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     //String a = '';
     MediaQueryData query = MediaQuery.of(context);
     return Scaffold(
-      appBar: AppBar(actions: []),
       backgroundColor: Colors.white,
-      body: SizedBox(
-        width: query.size.width,
-        height: query.size.height,
-        child: SingleChildScrollView(
-          child: Column(
+      body: Row(
+        children: [
+          Expanded(
+              child: Column(
             children: [
-              const Padding(padding: EdgeInsets.only(top: 20)),
+              _voltar(),
+              const Padding(padding: EdgeInsets.only(top: 22)),
+              _CardImage(query),
+              const Padding(padding: EdgeInsets.only(top: 12)),
               SizedBox(
                 width: query.size.width,
-                child: Card(
-                  shape: BeveledRectangleBorder(
-                    borderRadius: BorderRadius.circular(10.0),
-                  ),
-                  elevation: 10.0,
-                  child: Column(children: [
-                    Center(
-                      child: Text(
-                        widget.frase,
-                        style: const TextStyle(
-                            fontWeight: FontWeight.bold, color: Colors.black),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Padding(padding: EdgeInsets.only(left: 22)),
+                    Text(
+                      'Mariana Clubes',
+                      style: GoogleFonts.openSans(
+                        textStyle: const TextStyle(color: Colors.black),
+                        fontSize: 27,
+                        fontWeight: FontWeight.w700,
+                        //fontStyle: FontStyle.italic,
                       ),
                     ),
-                    SizedBox(
-                        width: 300,
-                        height: 150,
-                        child: Image.asset(widget.img)),
-                    const Padding(padding: EdgeInsets.only(top: 3))
-                  ]),
+                  ],
                 ),
               ),
-
-              //const Padding(padding: EdgeInsets.only(top: 68)),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  SizedBox(
-                    width: query.size.width,
-                    height: 130,
-                    child: Card(
-                      shape: BeveledRectangleBorder(
-                        borderRadius: BorderRadius.circular(10.0),
-                      ),
-                      elevation: 9.0,
-                      child: Row(
-                        children: [
-                          const Padding(padding: EdgeInsets.only(left: 12)),
-                          SizedBox(
-                            width: 226,
-                            child: Column(
-                              children: [
-                                const Padding(
-                                    padding: EdgeInsets.only(top: 13)),
-                                const Center(
-                                  child: Text(
-                                    "Voce",
-                                    style: TextStyle(
-                                        fontSize: 30,
-                                        fontWeight: FontWeight.bold),
-                                  ),
-                                ),
-                                const Center(
-                                  child: Text(
-                                    "Pode",
-                                    style: TextStyle(
-                                        fontSize: 25,
-                                        fontWeight: FontWeight.bold),
-                                  ),
-                                ),
-                                Center(
-                                  child: Text(
-                                    _variavel(),
-                                    style: const TextStyle(
-                                        fontSize: 18,
-                                        fontWeight: FontWeight.bold,
-                                        color: Colors.red),
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                          const Padding(padding: EdgeInsets.only(left: 66)),
-                          SizedBox(
-                              width: 80,
-                              height: 100,
-                              child: Card(
-                                elevation: 15.0,
-                                child: IconButton(
-                                    color: Colors.green[900],
-                                    enableFeedback: true,
-                                    onPressed: () {
-                                      _variavel();
-                                      setState(() {
-                                        widget.cont++;
-                                      });
-                                    },
-                                    icon: const Icon(Icons.refresh)),
-                              )),
-                        ],
-                      ),
-                    ),
-                  ),
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      const Padding(padding: EdgeInsets.only(top: 2)),
-                      SizedBox(
-                        width: query.size.width,
-                        height: 130,
-                        // ignore: deprecated_member_use
-                        child: GestureDetector(
-                          onTap: () {
-                            Navigator.pushNamed(context, '/');
-                          },
-                          child: Card(
-                              elevation: 2,
-                              child: Image.asset(
-                                'img/evento.png',
-                                fit: BoxFit.cover,
-                              )),
-                        ),
-                      ),
-                      const Padding(padding: EdgeInsets.only(left: 1)),
-                      SizedBox(
-                        width: query.size.width,
-                        height: 75,
-                        // ignore: deprecated_member_use
-                        child: Card(
-                          shape: BeveledRectangleBorder(
-                            borderRadius: BorderRadius.circular(10.0),
-                          ),
-                          elevation: 8.0,
-                          child: FlatButton(
-                            autofocus: true,
-                            textColor: Colors.white,
-                            color: Colors.green[900],
-                            onPressed: () async {
-                              await share();
-                              // Share.share('Hoje e dia de' + widget.roledehoje);
-                            },
-                            child: const Text('COMPARTILHE O ROLE DE HOJE'),
-                          ),
-                        ),
-                      ),
-                      SizedBox(
-                        height: 100, //ANUNCIO
-                        width: query.size.width,
-                      ),
-                      SizedBox(
-                        width: 200,
-                        height: 80,
-                        // ignore: deprecated_member_use
-                        child: RaisedButton(
-                          autofocus: true,
-                          textColor: Colors.white,
-                          color: Colors.white,
-                          onPressed: () {
-                            Navigator.pushNamed(context, '/');
-                          },
-                          child: const Text(
-                            'Voltar',
-                            style: TextStyle(color: Colors.black, fontSize: 22),
-                          ),
-                        ),
-                      ),
-                      //const Padding(padding: EdgeInsets.only(top: 52)),
-                    ],
-                  )
-                ],
-              ),
             ],
-          ),
-        ),
+          )),
+        ],
       ),
     );
   }
 
-  _geradorFamilia() {
-    int index = 0;
-    var random = Random();
-    List<String> lista = [
-      'Ir ao parque em familia',
-      'Jogar Uno com a familia',
-      'Ver um filme de comedia',
-      'Ver filme de Terror',
-      'Andar de bicicleta',
-      'Caminhar em familia',
-      'Ir em alguma cachoeira',
-      'Fazer churrasco',
-      'Meditar em familia',
-    ];
-    index = random.nextInt(lista.length);
-
-    return lista[index];
+  _voltar() {
+    return Row(crossAxisAlignment: CrossAxisAlignment.start, children: [
+      IconButton(
+        icon: const Icon(Icons.arrow_back),
+        color: Colors.black,
+        onPressed: () {},
+      )
+    ]);
   }
 
-  _gerador(String role) {
-    switch (role) {
-      case 'rolefamilia':
-        return _geradorFamilia();
-      case 'roleanimal':
-        return _geradorFamilia();
-      case 'rolenamoro':
-      case 'roleamigos':
-    }
+  _CardImage(MediaQueryData query) {
+    return Card(
+      elevation: 5,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(0.9),
+      ),
+      child: SizedBox(
+        height: 200,
+        width: query.size.width - 22,
+        child: Image.asset('img/amigos.png'),
+      ),
+    );
   }
 }
